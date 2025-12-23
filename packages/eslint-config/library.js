7cfae6fca58cf31c2092d49df469e39c2d5abd62
AQ-AzimSofi <muhammad.azim.iskandar.bin.mohd.sofi@gmail.com>
2025-12-23 17:24:47 +0900

    feat: initialize turborepo monorepo with pnpm workspaces
    - Add root package.json with turbo scripts
    - Configure pnpm workspaces for apps/* and packages/*
    - Add turbo.json build pipeline configuration
    - Create shared typescript-config package
    - Create shared eslint-config package
    - Add .env.example, .gitignore, .npmrc

---
+++ packages/eslint-config/library.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: ["plugin:@typescript-eslint/recommended", "prettier"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+    ],
+    "@typescript-eslint/no-explicit-any": "warn",
+  },
+};
